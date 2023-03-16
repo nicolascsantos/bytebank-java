2a7aa26aa86c66b79c9742af128f4207627f8777
Nicolas Carvalho <nicolas.carvalho@hotmail.com.br>
2023-03-15 22:11:19 -0300

    primeira conta criada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
     }
 }
